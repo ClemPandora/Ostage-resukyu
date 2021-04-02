1dde9f42d62befa8eaa562a70bc64109e767a704
Loann <courcoul66@gmail.com>
2021-04-02 12:01:32 +0200

    FinalScene

--- Assets/Scripts/Allies/Hostage/HostageLife.cs
+++ Assets/Scripts/Allies/Hostage/HostageLife.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class HostageLife : MonoBehaviour, Ally
@@ -11,13 +12,20 @@ public class HostageLife : MonoBehaviour, Ally
     public HostageFollowPlayer hostageFollowPlayer;
     public GameObject panelDead;
     public Slider healthBarHostage;
-    
+    private NavMeshAgent _nav;
+
+    private void Start()
+    {
+        _nav = GetComponent<NavMeshAgent>();
+    }
+
     public void Damage(int dmg) // Damage the player
     {
         life -= dmg;
         healthBarHostage.value = life;
         hostageFollowPlayer.enabled = false;
         hostageAi.enabled = true;
+        _nav.speed = 7;
         if (life <= 0)
         {
             Time.timeScale = 0f;
@@ -31,6 +39,7 @@ public class HostageLife : MonoBehaviour, Ally
     IEnumerator WaitForFollowPlayerAgain() // Wait for follow the player again 
     {
         yield return new WaitForSeconds(waitForSeconds);
+        _nav.speed = 3;
         hostageFollowPlayer.enabled = true;
         hostageAi.enabled = false;
     }
