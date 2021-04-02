3775ae9c9e1d2b7150e68176918ee7d455759216
Loann <courcoul66@gmail.com>
2021-04-02 09:49:37 +0200

    Push Menu

--- Assets/Scripts/UI/Restart.cs
+++ Assets/Scripts/UI/Restart.cs
@@ -13,4 +13,14 @@ public class Restart : MonoBehaviour
    {
       SceneManager.LoadScene(0);
    }
+
+   public void Play()
+   {
+      SceneManager.LoadScene(1);
+   }
+
+   public void QuitGame()
+   {
+      Application.Quit();
+   }
 }
