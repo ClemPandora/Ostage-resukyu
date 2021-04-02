6d9e156e03d5f5bfa3b6f416e7d21778b8dbde84
Lelio <lelio.rollin@e-artsup.net>
2021-04-02 11:26:59 +0200

    FirstAidKit Fix

--- Assets/Scripts/Items/FirstAidKitItem.cs
+++ Assets/Scripts/Items/FirstAidKitItem.cs
@@ -7,7 +7,7 @@ public class FirstAidKitItem : Item
 {
     public float healthAmount;
     
-    public void Use(Player player)
+    public override void Use(Player player)
     {
         player.health += healthAmount;
         base.Use(player);
